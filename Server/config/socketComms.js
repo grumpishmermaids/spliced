df9f90ac4a0d26d447fc2f99460f3302e8ed6fc0
mochuelo-kun <mochuelokun@gmail.com>
2015-10-08 13:47:15 -0700

    Fixed gameCode/room assignment (assigns client socket a .gameCode property)

--- Server/config/socketComms.js
+++ Server/config/socketComms.js
@@ -1,10 +1,7 @@
 var helpers = require('./helpers.js');  //not sure if need...
 var gameLogic = require('../game/gameLogic.js');
 
-
-
 var socketIo = require('socket.io');
-
 
 module.exports = function (app, server) {
 
@@ -16,46 +13,43 @@ module.exports = function (app, server) {
 
     // listen for host creating game, instantiate game instance on server, propagate start signal
     client.on('createGame', function (gameOptions) {
-      // var game = gameLogic.createGame(gameOptions);     // instantiate a game w/ game options
-      client.join('BEASTA');      // connect creator (this client) to socket room named their gameCode
-      io.to('BEASTA').emit('gameCreated', 'BEASTA');   // send to everyone in game (only host at this point) newly created game info
+      var game = gameLogic.createGame(gameOptions);     // instantiate a game w/ game options
+      console.log("made new game!");
+      console.dir(game);
+      client.gameCode = gameOptions.gameCode;
+      client.join(client.gameCode);      // connect creator (this client) to socket room named their gameCode
+      io.to(client.gameCode).emit('gameCreated', client.gameCode);   // send to everyone in game (only host at this point) newly created game info
     });
-
-    // client.on('createGame', function (gameOptions) {
-    //   // var game = gameLogic.createGame(gameOptions);     // instantiate a game w/ game options
-    //   client.join('BEASTA');      // connect creator (this client) to socket room named their gameCode
-    //   io.to('BEASTA').emit('gameCreated', 'BEASTA');   // send to everyone in game (only host at this point) newly created game info
-    // });
 
 
     // listen for players joining game, send them game info
-    client.on('joinGame', function (gameCode, playerOptions) {  // when new player joins a game
+    client.on('joinGame', function (data) {  // when new player joins a game
       console.log("'joinGame' ping from socket.id %s requesting gameCode %s", client.id, gameCode);
-      client.join(gameCode); // connect client to socket room named their gameCode
-      console.log('host client is in socket.rooms', client.rooms);
-      var game = gameLogic.addPlayerToGame(gameCode, playerOptions);
+      client.gameCode = data.gameCode;
+      client.join(client.gameCode); // connect client to socket room named their gameCode
+      console.log('player client now in room', client.gameCode);
+      // var game = gameLogic.addPlayerToGame(gameCode, playerOptions);
       if (!game) {
-        io.to(client.id).emit('joinFail', gameCode);     // send player attempting to join back a fail message
+        io.to(client.id).emit('joinFail', data.gameCode);     // send player attempting to join back a fail message
       } else {
-        io.to(client.id).emit('joinSuccess', {gameCode: gameCode});                         // send new player success ping
-        io.to(gameCode).emit('playerJoined', {gameCode: gameCode, player: playerOptions});  // send "playerJoined" to everyone in game
+        io.to(client.id).emit('joinSuccess', {gameCode: data.gameCode});                         // send new player success ping
+        io.to(gameCode).emit('playerJoined', {gameCode: data.gameCode, player: playerOptions});  // send "playerJoined" to everyone in game
       }
     });
 
 
     client.on('gameStart', function () {
       // propagate start event to all players in game
-      var gameCode = io.sockets.manager.roomClients[client.id];
-      io.to(gameCode).emit('start');
+      io.to(client.gameCode).emit('start');
 
       // initiate game timer as well
-      var countdown = gameLogic.getTimeLimit(gameCode);
+      var countdown = gameLogic.getTimeLimit(client.gameCode);
       if (countdown) {
         var timerId = setInterval(function () {  
           countdown--;
-          io.to(gameCode).emit('countdown', countdown);
+          io.to(client.gameCode).emit('countdown', countdown);
           if (countdown <= 0) {
-            io.to(gameCode).emit('end', null);  //TODO: endgame stuff
+            io.to(client.gameCode).emit('end', null);  //TODO: endgame stuff
             clearInterval(timerId);
           }
         }, 1000);
@@ -64,11 +58,10 @@ module.exports = function (app, server) {
 
 
     client.on('drawing', function (data) {
-      var gameCode = io.sockets.adapter.sids[client.id];
       console.log("'drawing' ping from socket.id %s in room %s", client.id, gameCode);
       console.log('drawer client is in socket.rooms', client.rooms);
-      console.dir(gameCode);
-      io.to(gameCode).emit('drawing', data);
+      console.dir(client.gameCode);
+      io.to(client.gameCode).emit('drawing', data);
     });
 
 
