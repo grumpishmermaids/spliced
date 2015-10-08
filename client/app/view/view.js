8a51b307ea00ba8ee3c2cbb2c9ffb227596570b2
hdh3000 <henry.hutcheson@gmail.com>
2015-10-07 19:37:24 -0700

    cleaned up and finalized view controller. Now handles setting up game state from sockets

--- client/app/view/view.js
+++ client/app/view/view.js
@@ -1,15 +1,35 @@
 angular.module('spliced.view', [])
 
-.controller('ViewController', function ($scope) {
+.controller('ViewController', function ($scope, Socket) {
   // drawing info will go here.
+  $scope.init = function() {
+    Socket.emit('gameInit');
+  };
+  
+  Socket.on('gameData', function(gameData){
+    for(var key in gameData){
+      scope.game.key = gameData.key;
+    }
+    //not sure we need this, but will force an update of dom...
+    $scope.$digest();
+  });
 
-  $scope.game = {};
-  $scope.game.prompt = "Horse";
-  $scope.game.timer = "00:10";
-  $scope.game.numPlayers = 4;
-  $scope.game.tiles = [[],[]];
-  $scope.game.drawing = undefined;
-  $scope.game.version = 0;
+  Socket.on('newPlayer', function(playerData){
+    players.push(playersData);
+    //not sure we need this, but will force an update of dom...
+    $scope.$digest();
+  });
+
+  $scope.game = {
+    prompt : null,
+    timer : null,
+    numDrawers : 1,
+    players : [],
+    version : 0 // for undo...
+  };
+
+  //make scope accessable to window for debugging
+  window.scope = $scope;
 
 })
 .directive('myView', ['Socket', function (Socket){
@@ -18,35 +38,38 @@ angular.module('spliced.view', [])
   var link = function(scope, element, attr) {
 
     var View = function(players, element) {
-      this.ctx = element.children()[0].getContext('2d');
-      console.log(this.ctx);
       this.players = players;
+
+      //creates the canvas on which to display images
+      this.ctx = element.children()[0].getContext('2d');
       this.ctx.canvas.width = element[0].clientWidth;
       this.ctx.canvas.height = element[0].clientHeight;
 
-      this.width_interval = (this.ctx.canvas.width/this.players)*2;
-      this.height_interval = this.ctx.canvas.height/2;
+      this.setIntervals();
 
-      this.drawBorder();
       if (this.players !== 1) {
-        this.drawHalf();
+        this.drawHalf(); 
       }
 
       this.drawSplits();
     };
 
-    // View.prototype.clearCanvas = function() {
-    //   this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-    // };
+    View.prototype.setIntervals = function() {
+      //the intervals are what we use to draw borders and set image size and position
+      if(this.players === 1){
+        this.width_interval = this.ctx.canvas.width;
+        this.height_interval = this.ctx.canvas.height;
+      } else if (this.players > 1) {
+        this.width_interval = (this.ctx.canvas.width/this.players)*2;
+        this.height_interval = this.ctx.canvas.height/2;
+      }
 
-    View.prototype.drawBorder = function() {
-      this.ctx.rect(20,20,this.ctx.canvas.width-40,this.ctx.canvas.height-40);
-      this.ctx.fillStyle = "white";
-      this.ctx.fill();
+
     };
 
     View.prototype.drawHalf = function() {
       this.ctx.beginPath();
+      this.ctx.lineWidth = 0.25;
       this.ctx.moveTo(0,this.ctx.canvas.height/2);
       this.ctx.lineTo(this.ctx.canvas.width,this.ctx.canvas.height/2); // Half separation
       this.ctx.stroke();
@@ -54,18 +77,21 @@ angular.module('spliced.view', [])
 
     View.prototype.drawSplits = function() {
       var interval = Math.round(this.ctx.canvas.width/this.players)*2;
-      for (var i = 0; i < this.players; i++) {
+      for (var i = 0; i < (this.players / 2) - 1 ; i++) {
         this.ctx.beginPath();
+        this.ctx.lineWidth = 0.25;
         this.ctx.moveTo(interval*(i+1),0);
         this.ctx.lineTo(interval*(i+1),this.ctx.canvas.height);
         this.ctx.stroke();
       }
     };
 
-    View.prototype.drawTile = function(x,y) {
+    View.prototype.drawTile = function(tile) {
+      var x = tile.x;
+      var y = tile.y;
       var img = new Image();
-      img.src = scope.game.tiles[x][y];
-      
+      img.src = tile.dataURL;
+
       this.ctx.drawImage(img,
         (x*this.width_interval), //sets top left corner of image relative to canvas top left
         (y*this.height_interval), // samezies
@@ -73,21 +99,19 @@ angular.module('spliced.view', [])
         (this.height_interval)); //height of image
     };
 
-    var view = new View(scope.game.numPlayers, element);
+    //initialize the view on the element when the players change...
+    scope.$watch('game.numDrawers', function(newVal) {
+      scope.view = new View(newVal, element);
+    });
 
+    //listen for drawing events on socket to update the view object
     Socket.on('drawing', function (tile) {
-      console.log("got data", tile);
-      scope.game.tiles[tile.x][tile.y] = tile.dataURL;
-      view.drawTile(tile.x, tile.y);
+      scope.view.drawTile(tile);
     });
   };
 
-  return {link : link};
+  return {
+    link : link
+  };
 
 }]);
-
-
-
-          // ((((x*this.width_interval)%(this.players/2)))*this.width_interval),
-          // (i*2 < this.players ? 0 : this.ctx.canvas.height/2), 
-          // this.width_interval, this.height_interval);
